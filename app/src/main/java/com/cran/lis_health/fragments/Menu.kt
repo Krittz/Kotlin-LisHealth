9c4c44f08bfc6bbbd37f9f5fa6b541a3452ac1f4
Cristian A. Silva <cristianalvessilvak@gmail.com>
2024-07-22 00:07:53 -0300

    Enviando mensagem

--- app/src/main/java/com/cran/lis_health/fragments/Menu.kt
+++ app/src/main/java/com/cran/lis_health/fragments/Menu.kt
@@ -1,6 +1,7 @@
 package com.cran.lis_health.fragments
 
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -10,11 +11,12 @@ import com.cran.lis_health.R
 import com.cran.lis_health.controller.SmsController
 import com.cran.lis_health.controllers.LocationController
 
-
 class Menu : Fragment() {
 
     private lateinit var smsController: SmsController
     private lateinit var locationController: LocationController
+    private var latitude: Double? = null
+    private var longitude: Double? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -27,18 +29,21 @@ class Menu : Fragment() {
     ): View? {
         val view = inflater.inflate(R.layout.fragment_menu, container, false)
 
-
         smsController = SmsController(requireContext(), requireActivity())
         locationController = LocationController(requireContext(), requireActivity())
 
         val btnAlert: AppCompatImageButton = view.findViewById(R.id.btnAlert)
         btnAlert.setOnClickListener {
-            locationController.getLastLocation { latitude, longitude ->
-                val phoneNumber = "5538999576845"
-                val message =
-                    "Olá, isso é um teste de envio de SMS. Localização: Latitude: $latitude, Longitude: $longitude"
-                smsController.sendSMS(phoneNumber, message)
+            locationController.getLastLocation { lat, lon ->
+                latitude = lat
+                longitude = lon
+                // Armazena as coordenadas para futura utilização
+                Log.d("LOCAL", "$latitude, $longitude")
+
             }
+            val phoneNumber = "5538999576845"
+            val message = "Olá, isso é um teste de envio de SMS. $latitude, $longitude"
+            smsController.sendSMS(phoneNumber, message)
         }
 
         return view
