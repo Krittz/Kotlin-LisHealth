530c7d02b4055f4047dcd9d2e4392e0f8f8b905b
Cristian A. Silva <cristianalvessilvak@gmail.com>
2024-07-22 00:41:04 -0300

    Removendo controller Location

--- app/src/main/java/com/cran/lis_health/fragments/Menu.kt
+++ app/src/main/java/com/cran/lis_health/fragments/Menu.kt
@@ -9,14 +9,11 @@ import androidx.appcompat.widget.AppCompatImageButton
 import androidx.fragment.app.Fragment
 import com.cran.lis_health.R
 import com.cran.lis_health.controller.SmsController
-import com.cran.lis_health.controllers.LocationController
 
 class Menu : Fragment() {
 
     private lateinit var smsController: SmsController
-    private lateinit var locationController: LocationController
-    private var latitude: Double? = null
-    private var longitude: Double? = null
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -30,19 +27,12 @@ class Menu : Fragment() {
         val view = inflater.inflate(R.layout.fragment_menu, container, false)
 
         smsController = SmsController(requireContext(), requireActivity())
-        locationController = LocationController(requireContext(), requireActivity())
+
 
         val btnAlert: AppCompatImageButton = view.findViewById(R.id.btnAlert)
         btnAlert.setOnClickListener {
-            locationController.getLastLocation { lat, lon ->
-                latitude = lat
-                longitude = lon
-                // Armazena as coordenadas para futura utilização
-                Log.d("LOCAL", "$latitude, $longitude")
-
-            }
             val phoneNumber = "5538999576845"
-            val message = "Olá, isso é um teste de envio de SMS. $latitude, $longitude"
+            val message = "Olá, isso é um teste de envio de SMS."
             smsController.sendSMS(phoneNumber, message)
         }
 
@@ -56,6 +46,6 @@ class Menu : Fragment() {
     ) {
         super.onRequestPermissionsResult(requestCode, permissions, grantResults)
         smsController.handlePermissionResult(requestCode, grantResults)
-        locationController.handlePermissionResult(requestCode, grantResults)
+
     }
 }
